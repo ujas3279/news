b0fdab030982da9c3c50cf8d55feef63216f9e99
ujas3279 <ujas6520@gmail.com>
2022-12-18 18:21:24 +0530

    initial

---
+++ Readme.md.txt
@@ -0,0 +1,5 @@
+Step to setup project in local
+
+step1- run command "npm install"
+
+step 2 - run command "npm start"
---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+//my routes
+const newsRoutes = require("./routes/news");
+
+
+
+
+//Middlewear
+ app.use(bodyParser.json());
+ app.use(cookieParser());
+ app.use(cors());
+
+//My Router
+app.use("/api", newsRoutes);
+
+//Port
+const port = process.env.PORT || 5000;
+
+//Starting server
+app.listen(port, () => {
+    console.log(`app is running at ${port}`);
+} );
+
+// DB connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("DB connected");
+}).catch(
+    console.log("DB not connected")
+);
